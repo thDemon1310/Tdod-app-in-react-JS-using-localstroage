753cd7c71ea2591b9c61cb67e00d228837bb5125
Utplaksh Mishra <umishra8770@gmail.com>
2025-07-01 03:22:15 +0530

    Added input div into componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import {} from "react";
 import "./App.css";
-
+import Input from "./components/Input/Input";
 function App() {
   return (
     <>
-      
+      <div className="bg-purple-950 w-full h-full flex items-center justify-center">
+        <div className="flex items-center justify-center flex-col border w-150 min-h-150">
+          <nav>
+            <h1>TODO List</h1>
+          </nav>
+          <Input />
+        </div>
+      </div>
     </>
   );
 }
