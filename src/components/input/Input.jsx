7e34b8ddd183337326a84844162f524001a3e645
Utplaksh Mishra <umishra8770@gmail.com>
2025-07-13 12:33:26 +0530

    less worse code the before but there is issue when we reload the webpage

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,35 +1,34 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Input = () => {
-  const [Todo, setTodo] = useState("");
-  const todoData_Id = useRef(0);
-  const handleInput = (event) => {
-    setTodo(event.target.value);
+  const [inputData, setInputData] = useState("");
+  const todoId = useRef(0);
+  const [Todo, setTodo] = useState({
+    id: Number,
+    task: String,
+    isDone: Boolean,
+  });
+
+  useEffect(() => {
+    const key = `Task${Todo.id}`;
+    const value = JSON.stringify(Todo);
+    localStorage.setItem(key, value);
+  }, [Todo]);
+
+  const handleClick = () => {
+    setTodo({
+      id: handleTodoId(),
+      task: inputData,
+      isDone: false,
+    });
   };
-  const assId = async () => {
+
+  const handleTodoId = () => {
+    todoId.current = todoId.current + 1;
     if (localStorage.length !== 0) {
       return localStorage.length + 1;
     } else {
-      return todoData_Id.current + 1;
-    }
-  };
-  const saveLocalStorage = async () => {
-    if (Todo !== "") {
-      const aboutTodo = {
-        task: Todo,
-        isDone: false,
-        id: await assId(),
-      };
-      try {
-        let Task = `task${aboutTodo.id}`;
-        localStorage.setItem(Task, JSON.stringify(aboutTodo));
-        console.log(localStorage.getItem(Task));
-      } catch (err) {
-        console.error(err);
-        console.log(`Data not save to loacltorage`);
-      }
-    } else {
-      console.log(`Please enter the input`);
+      return todoId.current;
     }
   };
   return (
@@ -38,13 +37,16 @@ const Input = () => {
         type="text"
         name="todoInput"
         id=""
+        value={inputData}
         className="border rounded-3xl p-2 w-full text-[20px]"
         placeholder="Enter your Todo here!!"
-        onChange={handleInput}
+        onChange={(event) => {
+          setInputData(event.target.value);
+        }}
       />
       <button
         className="border-0 bg-blue-800 text-white p-3 rounded-2xl font-bold"
-        onClick={saveLocalStorage}
+        onClick={handleClick}
       >
         ADD
       </button>
